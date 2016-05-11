9fdcabf73d1f937d4c1595af8fbeb444ea6bc623
Tad <t.slaff@inovancetech.com>
2016-05-11 13:49:34 -0400

    Reverted to initial version

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:California1@flasktest.ciifl3fa1g9q.us-west-2.rds.amazonaws.com:3306/Flaskdb'
 
 # Uncomment to work with local db
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 
 SQLALCHEMY_POOL_RECYCLE = 3600
 
