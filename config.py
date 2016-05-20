4fc49c9387dd37876b6f95b254401f936ad3175c
Tad <t.slaff@inovancetech.com>
2016-05-20 14:20:31 -0400

    Connected to mysql AWS db, printed values as string

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:California1@flasktest.ciifl3fa1g9q.us-west-2.rds.amazonaws.com:3306/Flaskdb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:California1@flasktest.cf4uesukkwab.us-east-1.rds.amazonaws.com:3306/flaskdb'
 
 # Uncomment to work with local db
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
