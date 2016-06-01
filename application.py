6648f11d9396b6d91ed46688373a32df0956db07
Tad <t.slaff@inovancetech.com>
2016-06-01 17:30:15 -0400

    Updated data base, initial work on d3 chart

--- application.py
+++ application.py
@@ -13,32 +13,29 @@ import pandas as pd
 import numpy as np
 from application import db
 from application.models import Data
+import random
 
 # Elastic Beanstalk initalization
+
 application = Flask(__name__)
 application.debug=True
 application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 
 conn = MySQLdb.connect(host="flasktest.cf4uesukkwab.us-east-1.rds.amazonaws.com", user="flask", passwd="California1", db="flaskdb");
 cursor = conn.cursor();
-sql = 'SELECT Date_o, Product, Company, State, Zip_code FROM Data_1'
+sql = 'SELECT PROP_FRONT,PROP_DEPTH FROM Commercial_Data LIMIT 30'
 df = pd.read_sql(sql, conn)
 conn.close()
 
-#application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask:California1@flasktest.cf4uesukkwab.us-east-1.rds.amazonaws.com:3306/flaskdb'
 
 
 @application.route('/')
 #@application.route('/index', methods=['GET', 'POST'])
-#def hello_world():
 
-	#string = str(df)[0:500]
-	
-	#return string
 
 def analysis():
-    x = pd.DataFrame(df)
-    return render_template("index.html", name='this is my name', data=x.to_html())	
+    d = pd.DataFrame(df)
+    return render_template("index.html", name='Commercial Data', data=d.to_json())	
 	
     
     
